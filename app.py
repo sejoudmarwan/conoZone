1435f1aeab6a5d53125108bf6756f6036515e077
Sejoud <sagymohamed63@gmail.com>
2026-07-21 23:30:37 +0300

    Refactor file structure, migrate to Flask, and convert static pages to Jinja templates

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def home ():
+  return render_template('index.html')
+
+@app.route("/cart")
+def cart ():
+  return render_template('cart.html')
+
+@app.route('/login')
+def login ():
+  return render_template('login.html')
+
+@app.route('/menu')
+def menu ():
+  return render_template('menu.html')
+
+@app.route('/signup')
+def signup ():
+  return render_template('Singup.html')
+
+if __name__=="__main__":
+  app.run(host="0.0.0.0",port=5000,debug=True)
